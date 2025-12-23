d6d3d6cc92c37b1ceff191de83a8d47aae195108
Byron Karlen <byronkarlen@gmail.com>
2025-12-23 00:31:14 -0700

    wip

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,47 @@ vim.keymap.set({ "n", "x" }, "<leader>d", '"_d', { desc = "Delete without yankin
 vim.keymap.set("n", "<leader>D", '"_D', { desc = "Delete to EOL without yanking" })
 vim.keymap.set({ "n", "x" }, "<leader>c", '"_c', { desc = "Change without yanking" })
 vim.keymap.set("n", "<leader>C", '"_C', { desc = "Change to EOL without yanking" })
+
+-- copy relative file path + visual line range to system clipboard
+vim.keymap.set("v", "<leader>yy", function()
+  local l1 = vim.fn.line("v") -- visual anchor
+  local l2 = vim.fn.line(".") -- cursor
+
+  local start_line = math.min(l1, l2)
+  local end_line = math.max(l1, l2)
+
+  local path = vim.fn.expand("%:p:.")
+  if path == "" then
+    return
+  end
+
+  local ref
+  if start_line == end_line then
+    ref = path .. ":" .. start_line
+  else
+    ref = path .. ":" .. start_line .. "-" .. end_line
+  end
+
+  vim.fn.setreg("+", ref)
+
+  vim.notify(ref, vim.log.levels.INFO, { title = "Copied" })
+
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+end, {
+  desc = "Copy relative file path + line range",
+  silent = true,
+})
+
+-- copy relative file path to system clipboard
+vim.keymap.set("n", "<leader>yy", function()
+  local path = vim.fn.expand("%:p:.")
+  vim.notify(path, vim.log.levels.INFO, { title = "Copied" })
+  vim.fn.setreg("+", path)
+end, { desc = "Copy relative file path" })
+
+-- copy absolute file path to system clipboard
+vim.keymap.set("n", "<leader>yz", function()
+  local path = vim.fn.expand("%:p")
+  vim.notify(path, vim.log.levels.INFO, { title = "Copied" })
+  vim.fn.setreg("+", path)
+end, { desc = "Copy absolute file path" })
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,7 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<leader>bH", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer left" },
+    { "<leader>bL", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer right" },
+  },
+}
