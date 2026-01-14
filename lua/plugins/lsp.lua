83e2de99ae223a85542827dd48baef153e5f9f97
Byron Karlen <byronkarlen@gmail.com>
2026-01-14 12:22:51 -0700

    configure eslint lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,19 @@ return {
             client.server_capabilities.documentRangeFormattingProvider = false
           end,
         },
+        eslint = {
+          -- This is needed because volta prepends resolved binary paths before launching. Then, when a node spawns another process
+          -- with a different cwd (or it changes into that cwd), it doesn't matter because the resolved node binary is already
+          -- at the front of PATH. This means that when launching nvim from universe, it will use univere's node version to execute
+          -- the LSP even though its executing it in the server/website directory.
+
+          -- Use default global node version. Prepends homebrew's version of node to path since login shell
+          cmd = { "bash", "-lc", "exec vscode-eslint-language-server --stdio" },
+          settings = {
+            workingDirectories = { mode = "location" },
+            workingDirectory = { mode = "location" }, -- affects the process's cwd for the eslint run, not eslint walking upward for configs
+          },
+        },
       },
     },
   },
