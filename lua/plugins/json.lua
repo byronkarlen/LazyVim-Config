9c71c68abf7005e3436d14bb51eb9a4135d93b38
Byron Karlen <byronkarlen@gmail.com>
2026-05-25 10:20:27 -0700

    fix jsonls lsp

---
+++ lua/plugins/json.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        jsonls = {
+          before_init = function(_, new_config)
+            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+            vim.list_extend(
+              new_config.settings.json.schemas,
+              require("schemastore").json.schemas({
+                ignore = { "Expo SDK" },
+              })
+            )
+          end,
+        },
+      },
+    },
+  },
+}
