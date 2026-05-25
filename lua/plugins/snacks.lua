42c130d13d4f93e7731db2b002c2bba965cc6e39
Byron Karlen <byronkarlen@gmail.com>
2026-05-25 10:35:23 -0700

    show hidden files by default

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,6 +5,11 @@ return {
     opts = function(_, opts)
       opts.indent = opts.indent or {}
       opts.indent.enabled = false
+
+      opts.picker = opts.picker or {}
+      opts.picker.sources = opts.picker.sources or {}
+      opts.picker.sources.explorer = opts.picker.sources.explorer or {}
+      opts.picker.sources.explorer.hidden = true
     end,
   },
 }
