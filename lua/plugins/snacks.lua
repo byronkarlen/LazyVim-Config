aae14513c3618a0247deac196e278e1503ddb6b6
Byron Karlen <byronkarlen@gmail.com>
2026-05-30 13:59:39 -0700

    tweaks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,8 +8,22 @@ return {
 
       opts.picker = opts.picker or {}
       opts.picker.sources = opts.picker.sources or {}
+
+      opts.picker.sources.files = opts.picker.sources.files or {}
+      opts.picker.sources.files.hidden = true
+      opts.picker.sources.files.ignored = false
+
+      opts.picker.sources.grep = opts.picker.sources.grep or {}
+      opts.picker.sources.grep.hidden = true
+      opts.picker.sources.grep.ignored = false
+
+      opts.picker.sources.grep_word = opts.picker.sources.grep_word or {}
+      opts.picker.sources.grep_word.hidden = true
+      opts.picker.sources.grep_word.ignored = false
+
       opts.picker.sources.explorer = opts.picker.sources.explorer or {}
       opts.picker.sources.explorer.hidden = true
+      opts.picker.sources.explorer.ignored = false
     end,
   },
 }
