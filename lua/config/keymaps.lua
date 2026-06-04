7f1c698175a803f64689acc3e1ce9041127cc40d
Byron Karlen <byronkarlen@gmail.com>
2026-06-04 08:18:39 -0700

    map space space to cwd not root

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local LazyVim = require("lazyvim.util")
+
+vim.keymap.set("n", "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
+
 -- Delete without yanking (to black hole register)
 vim.keymap.set({ "n", "x" }, "<leader>d", '"_d', { desc = "Delete without yanking" })
 vim.keymap.set("n", "<leader>D", '"_D', { desc = "Delete to EOL without yanking" })
