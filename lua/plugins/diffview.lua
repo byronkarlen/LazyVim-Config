4ad187bb7f71ce486855b64d02dd842aff99e7ef
Byron Karlen <byronkarlen@gmail.com>
2026-06-14 13:05:17 -0700

    tweak diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,6 +3,7 @@ return {
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
   opts = {},
   keys = {
+    { "<leader>gj", ":DiffviewOpen ", desc = "Diffview Open (custom)" },
     { "<leader>gk", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
     { "<leader>gl", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
   },
